838792f7a19d246c5e3333b0e72a751724b9d083
Shardul Singh <shardulvikramsinghh@gmail.com>
2022-01-07 23:35:39 +0530

    Updated Screen UI ELements

--- src/views/LandingScreen/index.tsx
+++ src/views/LandingScreen/index.tsx
@@ -1,21 +1,53 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {Screens} from '../../constants';
-import {useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {RootStackListType} from '../../../App';
+import {Button} from '../../components';
 
 const LandingScreen: React.FunctionComponent = () => {
   const navigator = useNavigation<StackNavigationProp<RootStackListType>>();
+
+  const navigateToQuizScreen = () => {
+    navigator.navigate(Screens.QUIZ_SCREEN);
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Landing Page</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Image source={require('../../assets/brain.png')} style={styles.image} />
+      <Text style={styles.landingPageLabel}>Quiz Trivia!</Text>
+      <Text style={styles.subLabel}>All trivia collides here.</Text>
       <Button
-        title="Go"
-        onPress={() => navigator.navigate(Screens.QUIZ_SCREEN)}
+        label="Start"
+        testID="gotoQuizButton"
+        onPress={navigateToQuizScreen}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  landingPageLabel: {
+    fontSize: 42,
+    color: '#0d0630',
+    fontFamily: 'Arial',
+    fontWeight: '500',
+    marginTop: 30,
+  },
+  subLabel: {
+    fontSize: 20,
+    marginTop: 8,
+    marginBottom: 164,
+    color: 'black',
+    fontFamily: 'Arial',
+  },
+  image: {width: '70%', height: '40%', resizeMode: 'contain'},
+});
 
 export default LandingScreen;
--- src/views/QuizScreen/index.tsx
+++ src/views/QuizScreen/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {getRandomQuizQuestion} from '../../services/QuizService';
+import {Question} from '../../@types/network';
+import {LoadingIndicator} from '../../components';
+import QuizComponent from './QuizComponent';
 
 const QuizScreen: React.FC = () => {
+  const [quizData, setQuizData] = useState<Question | null>(null);
+  useEffect(() => {
+    getRandomQuizQuestion().then(response => {
+      console.log(response.data);
+      setQuizData(response.data);
+    });
+  }, []);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Quiz Page</Text>
+    <View style={styles.quizContainer}>
+      {quizData ? <QuizComponent data={quizData} /> : <LoadingIndicator />}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  quizContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // marginLeft: 10,
+    alignSelf: 'center',
+    // backgroundColor: 'red',
+    width: '94%',
+  },
+});
 
 export default QuizScreen;
