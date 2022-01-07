1a69bfe9c6925483568b9505b6785ff5f07bc578
Shardul Singh <shardulvikramsinghh@gmail.com>
2022-01-07 23:33:17 +0530

    Added LoadingIndicator

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,8 @@
+import {ActivityIndicator} from 'react-native';
+import React from 'react';
+
+const LoadingIndicator: React.FC = () => (
+  <ActivityIndicator size="large" color="#00CED0" />
+);
+
+export default LoadingIndicator;
